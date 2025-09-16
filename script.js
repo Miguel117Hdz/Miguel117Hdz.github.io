4caada7ca1c84acb68b0e5b5dafdfa37e8ffd4d4
Miguel117Hdz <21042@virtual.utsc.edu.mx>
2025-09-15 20:27:37 -0600

    Sitio Profecional

---
+++ script.js
@@ -0,0 +1,9 @@
+// Pequeña animación de scroll suave para links internos
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e){
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
